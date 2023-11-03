85d539927c317aa6fa71463810e5e294f14c973b
pavlo-boichuk <betonsuccess@gmail.com>
2023-11-04 00:07:44 +0200

    c4

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-print("Hello all world")
+#print("Hello all world")
+#print("Hello Git")
 
-print("Hello Git")
+dict1 = dict()
+
+dict2 = {
+   "key": "value",
+    1: "one",
+    2: 2,
+}
+
+dict3 = dict()
+dict3 = {"key": "value"}
+dict3["new_key"] = "new value"
+
+print("dict1", dict1)
+print("dict2", dict2)
+print("dict3", dict3)
